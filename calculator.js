cdd6adc69553a8f327a5084bc69f1e0a3f5ed392
Andre Santos <andresantosas2558487@gmail.com>
2021-10-26 23:19:13 -0300

    trabalhando com módulos no node

---
+++ calculator.js
@@ -0,0 +1,17 @@
+let somaFunc = require("./somar")
+
+const subFunc = require("./subtrair")
+
+let multFunc = require("./multiplicar")
+
+let divFunc = require("./dividir")
+
+console.log(somaFunc(130, 3))
+
+console.log(subFunc(111, 3))
+
+console.log(multFunc(341, 3))
+
+console.log(divFunc(10, 3))
+
+
